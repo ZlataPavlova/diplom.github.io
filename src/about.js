ae725f8b2d14651c0ecf9e54ad07eafa1c8db9df
Zlata Pavlova <PavlovaZV1@yandex.ru>
2020-04-10 19:35:33 +0300

    загрузка изображений

--- src/about.js
+++ src/about.js
@@ -1,11 +1,10 @@
 import "./about.css";
-import './image/о_проекте.svg';
-import './images/avatar.jpg';
-import './image/css.svg';
-import './image/html.svg';
-import './image/js2.png';
-import './image/slider-back.png';
-import './image/slider-next.png';
-import './image/vk.svg';
+// import './image/о_проекте.svg';
+// import './images/avatar.jpg';
+// import './image/css.svg';
+// import './image/html.svg';
+// import './image/js2.png';
+// import './image/slider-back.png';
+// import './image/slider-next.png';
 
-import { preloader } from "./blocks/preloader/preloader"
+// import { preloader } from "./blocks/preloader/preloader"
