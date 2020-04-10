43681def8dcb8cb7ddb0b8f3cac6adc5a6cd46d2
Zlata Pavlova <PavlovaZV1@yandex.ru>
2020-04-10 17:55:39 +0300

    fourth commit

--- src/about.js
+++ src/about.js
@@ -1,3 +1,11 @@
 import "./about.css";
+import './image/о_проекте.svg';
+import './image/avatar.jpg';
+import './image/css.svg';
+import './image/html.svg';
+import './image/js2.png';
+import './image/slider-back.png';
+import './image/slider-next.png';
+import './image/vk.svg';
 
 import { preloader } from "./blocks/preloader/preloader"
--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 import "./style.css";
+
+
+import './image/главная.svg';
+
+
+import './image/avatar.jpg';
+import './image/card_1.png';
+import './image/card_2.png';
+import './image/card_3.png';
+import './image/card_4.png';
+import './image/card_5.png';
+import './image/card_6.png';
+
+import './image/git.svg';
+import './image/header.png';
+
+import './image/not-found_v1.svg';
+
+
+import './image/vk.svg';
+import './image/webpack.svg';
