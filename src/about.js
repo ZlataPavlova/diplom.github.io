b19ab103c40a449bc317e77cdbc79d6524c5079a
Zlata Pavlova <PavlovaZV1@yandex.ru>
2020-04-10 16:57:18 +0300

    third commit

--- src/about.js
+++ src/about.js
@@ -1,31 +1,3 @@
 import "./about.css";
 
-
-const swiperContainer = document.querySelector('.swiper-container');
-const swiperPagination = document.querySelector('.swiper-pagination');
-const swiperButtonNext = document.querySelector('.swiper-button-next');
-const swiperButtonPrev = document.querySelector('.swiper-button-prev');
-const swiperScrollbar = document.querySelector('.swiper-scrollbar');
-
-import Swiper from 'swiper';
-
-var mySwiper = new Swiper('.swiper-container', { // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-
-    // If we need pagination
-    pagination: {
-        el: '.swiper-pagination',
-    },
-
-    // Navigation arrows
-    navigation: {
-        nextEl: swiperButtonNext,
-        prevEl: swiperButtonPrev,
-    },
-
-    // And if we need scrollbar
-    scrollbar: {
-        el: swiperScrollbar,
-    },
-});
+import { preloader } from "./blocks/preloader/preloader"
